54aba295cd7bbb51498bed9e4568bce3d46db7bb
diyathr <diyathr@zone24x7.com>
2021-09-24 19:21:44 +0530

    initial commit

---
+++ CalendarSync_Blazor_WA/CalendarSync_Blazor_WA/Shared/CalendarSyncConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalendarSync_Blazor_WA.Shared
+{
+    public class CalendarSyncConfiguration
+    {
+        public string ApplicationId { get; set; }
+        public string DirectoryId { get; set; }
+        public string ServiceAccountEmail { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ CalendarSync_Blazor_WA/CalendarSync_Blazor_WA/Shared/Extenstions.cs
@@ -0,0 +1,22 @@
+﻿using System.Security;
+
+namespace CalendarSync_Blazor_WA.Shared
+{
+    public static class Extenstions
+    {
+        public static SecureString ToSecureString(this string Source)
+        {
+            if (string.IsNullOrWhiteSpace(Source))
+                return null;
+            else
+            {
+                SecureString Result = new SecureString();
+                foreach (char c in Source)
+                {
+                    Result.AppendChar(c);
+                }
+                return Result;
+            }
+        }
+    }
+}
